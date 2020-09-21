6eabb3245f23a016a54467535e53910a8a263a4d
Luan-Augusto <luanalencar134@gmail.com>
2020-09-21 01:20:15 -0300

    first commit

---
+++ src/in/augustluan/entidades/Produto.java
@@ -0,0 +1,42 @@
+package in.augustluan.entidades;
+
+public class Produto {
+
+	private String sku;
+	private String nome;
+
+	public Produto(String sku, String nome) {
+		this.sku = sku;
+		this.nome = nome;
+	}
+
+	public boolean equals(Object obj) {
+		Produto outro = (Produto) obj;
+		return this.sku.equals(outro.getSku());
+	}
+
+	public Produto() {
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [sku=" + sku + ", nome=" + nome + "]";
+	}
+
+}
